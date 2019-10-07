a26d51592c3a72776873b473dd5e470c023b5b71
Muhamad Fahdy <fahdy173040030@mail.unpas.ac.id>
2019-10-07 19:31:08 +0700

    membuat properti untuk kelas app

--- app/core/App.php
+++ app/core/App.php
@@ -1,14 +1,19 @@
 <?php 
 
 class App{
+	protected $controller = 'Home';
+	protected $method = 'index';
+	protected $params = [];
 	public function __construct(){
 		$url = $this->parseURL();
-		var_dump($_GET);
+		var_dump($url);
 	}
 
 	public function parseURL(){
 		 if (isset($_GET['url']) ) {
-		 	$url = $_GET['url'];
+		 	$url =  rtrim( $_GET['url'], '/');
+		 	$url = filter_var($url, FILTER_SANITIZE_URL);
+		 	$url = explode('/', $url); 
 		 	return $url; 
 		 }
 	}
