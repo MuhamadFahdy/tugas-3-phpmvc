2cf35906abcf18947c6db98ee29d4159e7609aed
Muhamad Fahdy <fahdy173040030@mail.unpas.ac.id>
2019-10-08 20:59:48 +0700

    menambahkan User_model.php di folder models

---
+++ app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php 
+
+class User_model{
+	private $nama = 'Fahdy';
+
+	public function getUser(){
+		return $this->nama;
+	}
+}
+ ?>}
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,5 @@
 <div class="container">
 	<h1 class="mt-4">About Me</h1>
 	<img src="<?= BASEURL; ?>/img/silvia.jpg" alt="Silvia s15" width="250" class="rounded-circle shadow">
-	<p>Halo, nama saya <?= $data['nama']; ?> ,umur saya <?= $data['umur']; ?>  ,saya adalah <?= $data['pekerjaan']; ?>  </p>
+	<p>Halo, nama saya <?= $data['nama']; ?> ,umur saya <?= $data['umur']; ?>,saya adalah <?= $data['pekerjaan']; ?>  </p>
 </div>
