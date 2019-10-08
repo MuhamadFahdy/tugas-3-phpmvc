631018f930ee33f331124197d85fad9bfb7eb60e
Muhamad Fahdy <fahdy173040030@mail.unpas.ac.id>
2019-10-08 23:27:53 +0700

    membuat Mahasiswa_model.php di folder models

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -36,11 +36,12 @@ class Mahasiswa_model{
 	// 	]
 	// ];
 
-	public function getAllMahasiswa(){
+	public function getAllMahasiswa()
+	{
 		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
 		$this->stmt->execute();
 		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 }
 
- ?> 
+ 
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -3,7 +3,7 @@
 	<div class="row">
 		<div class="col-6">
 			<h3>Daftar Mahasiswa</h3>
-			<?php foreach ($data['mhs'] as $mhs) : ?>
+			<?php foreach ( $data['mhs'] as $mhs) : ?>
 			<ul>
 				<li><?= $mhs['nama']; ?></li>
 				<li><?= $mhs['nrp']; ?></li>
