fd3f3599617aefdcaa99c6a0ad390abfe25e51f5
Muhamad Fahdy <fahdy173040030@mail.unpas.ac.id>
2019-10-09 20:20:42 +0700

    menambahkan function query,bind,execute resultSet,dan singel di Database.php

--- app/core/Database.php
+++ app/core/Database.php
@@ -11,13 +11,55 @@ class Databse{
 
 	public function __construct(){
 		//data source name
-		$dsn = 'mysql:host=localhost;dbname=phpmvc';
+		$dsn = 'mysql:host='.$this->host.';dbname='.$this->db_name;
+
+		$option =[
+			PDO::ATTR_PERSISTENT => true,
+			PDO::ATTR_ERRORMODE => PDO::ERRMODE_EXCEPTION
+		];
 
 		try{
-			$this->dbh = new PDO($dsn,'root','');
+			$this->dbh = new PDO($dsn,$this->user,$this->pass,$option);
 		} catch(PDOException $e){
 			die($e->getMessage());
+		} 
+	}
+
+	public function query($query){
+		$this->stmt = $this->dbh->prepare($quer);
+	} 
+
+	public function bind($param,$value,$type = null){
+		if (is_null($type)) {
+			switch (true) {
+				case is_int($value):
+					$type = PDO::PARAM_INT;
+					break;
+				case is_bool($value):
+					$type = PDO::PARAM_BOOL;
+					break;
+				case is_null($value):
+					$type = PDO::PARAM_NUUL;
+					break;
+				default :
+					$type = PDO::PARRAM_STR;
+			}
 		}
+		$this->stmt->bindValue($param,$value,$type);
+	}
+
+	public function execute(){
+		$this->stmt->execute();
+	}
+
+	public function resultSet(){
+		$this->execute();
+		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+	public function single(){
+		$this->execute();
+		return $this->stmt->fetch(PDO::FETCH_ASSOC);
 	}
 }
 
