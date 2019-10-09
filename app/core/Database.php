44c174eaac3ec79250aaf2ebc2ffcb1cadbf2271
Muhamad Fahdy <fahdy173040030@mail.unpas.ac.id>
2019-10-09 19:57:15 +0700

    memindahkan isi __construct dari Mahasiswa_model ke Database.php

--- app/core/Database.php
+++ app/core/Database.php
@@ -8,6 +8,17 @@ class Databse{
 
 	private $dbh;
 	private $stmt;
+
+	public function __construct(){
+		//data source name
+		$dsn = 'mysql:host=localhost;dbname=phpmvc';
+
+		try{
+			$this->dbh = new PDO($dsn,'root','');
+		} catch(PDOException $e){
+			die($e->getMessage());
+		}
+	}
 }
 
  ?>
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,25 +1,14 @@
 <?php 
 
 class Mahasiswa_model{
-	private $dbh; //databse handler
-	private $stmt; //untuk menampung statemen database
-
-	public function __construct(){
-		//data source name
-		$dsn = 'mysql:host=localhost;dbname=phpmvc';
-
-		try{
-			$this->dbh = new PDO($dsn,'root','');
-		} catch(PDOException $e){
-			die($e->getMessage());
-		}
-	}
+	
+	
 
 	public function getAllMahasiswa()
 	{
 		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
 		$this->stmt->execute();
-		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $this->stmt->fetchAll(PDO::FETCH_ ASSOC);
 	}
 }
 
